934560343442433405c62a3475ba774ac200853c
Sarah Tolley <stolley@ucla.edu>
2015-01-27 02:09:49 +0000

    print length of fragments

--- restriction_frag_len.py
+++ restriction_frag_len.py
@@ -6,3 +6,7 @@ my_dna = "ACTGATCGATTACGTATAGTAGAATTCTATCATACATATATATCGATGCGTTCAT"
 frag1_length = my_dna.find("GAATTC") + 1
 frag2_length = len(my_dna) - frag1_length
 
+#print lengths
+#need to use str() function to print numbers as a string 
+print("length of fragment one is " + str(frag1_length))
+print("length of fragment two is " + str(frag2_length))
