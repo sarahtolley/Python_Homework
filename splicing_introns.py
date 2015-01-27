40ff4e7795048f7bcc29bb353695fa9a4cfb3efa
Sarah Tolley <stolley@ucla.edu>
2015-01-27 04:34:08 +0000

    find percent of my_dna that is coding

--- splicing_introns.py
+++ splicing_introns.py
@@ -1,6 +1,8 @@
 #splicing out introns, parts 1-3, exercise 4
 
+from __future__ import division 
 my_dna = "ATCGATCGATCGATCGACTGACTAGTCATAGCTATGCATGTAGCTACTCGATCGATCGATCGATCGATCGATCGATCGATCGATCATGCTATCATCGATCGATATCGATGCATCGACTACTAT"
+
 
 #part 1
 #designate exons in sequence
@@ -8,4 +10,11 @@ exon1 = my_dna[0:63]
 exon2 = my_dna[90: ]
 #print coding regions of the DNA sequence 
 print(exon1 + exon2)
+
+#part 2
+#calculate what percentage of the DNA sequence is coding
+coding_length = len(exon1 + exon2)
+total_length = len(my_dna)
+print(100 * coding_length / total_length)
+
 
