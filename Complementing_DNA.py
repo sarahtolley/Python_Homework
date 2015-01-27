aec42a119ea1f278bc4d81a53416108653c33f1c
Sarah Tolley <stolley@ucla.edu>
2015-01-27 02:00:41 +0000

    Only print completed complementary sequence

--- Complementing_DNA.py
+++ Complementing_DNA.py
@@ -5,15 +5,10 @@ my_dna = "ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT"
 
 #replace my_dna with complement
 
-replacement1 = my_dna.replace('A', 't')
-#need to print it for the command to be useful
-print(replacement1) 
+replacement1 = my_dna.replace('A', 't') 
 replacement2 = replacement1.replace('T','a')
-print(replacement2)
 replacement3 = replacement2.replace('C', 'g')
-print(replacement3)
 replacement4 = replacement3.replace('G', 'c')
-print(replacement4)
 
 #change complementary strand to uppercase
 print(replacement4.upper())
