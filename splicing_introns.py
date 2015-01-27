9c4ffa0ab28bc08b465c4578f2def510660b424b
Sarah Tolley <stolley@ucla.edu>
2015-01-27 04:46:48 +0000

    Part 3, splicing out introns

--- splicing_introns.py
+++ splicing_introns.py
@@ -9,12 +9,16 @@ my_dna = "ATCGATCGATCGATCGACTGACTAGTCATAGCTATGCATGTAGCTACTCGATCGATCGATCGATCGATCG
 exon1 = my_dna[0:63]
 exon2 = my_dna[90: ]
 #print coding regions of the DNA sequence 
-print("Coding regions of the DNA sequence = " exon1 + exon2)
+print("Part 1 : " + str(exon1 + exon2))
 
 #part 2
 #calculate what percentage of the DNA sequence is coding
 coding_length = len(exon1 + exon2)
 total_length = len(my_dna)
-print("Percentage of the DNA that is coding = "100 * coding_length / total_length)
+print("Part 2 : " + str(100 * coding_length / total_length))
 
 
+#part 3
+#splicing out introns
+intron = my_dna[90: ]
+print("Part 3 : " + str(exon1 +intron.lower() +exon2))
