a6c3295adde78804b1266b86d79a2e4eaa1f82d3
Sarah Tolley <stolley@ucla.edu>
2015-01-27 01:31:34 +0000

    print number of A and T

--- AT_content.py
+++ AT_content.py
@@ -4,3 +4,12 @@ from __future__ import division
 
 my_dna = "ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT"
 
+
+#count A and T
+length = len(my_dna)
+a_count = my_dna.count('A')
+t_count = my_dna.count('T')
+
+#print number of A and T
+at = (a_count + t_count) / length
+print("AT content is " + str(at))
